2a12e4cd40a31e3cc94bebb807ac5b8db1ee4685
ericaterkhorn <erica.terkhorn@gmail.com>
2020-12-16 20:34:35 -0500

    crime create crime list item crime get

---
+++ TrueCrimeRepo.Services/CrimeService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrueCrimeRepo.Data;
+using TrueCrimeRepo.Models;
+
+namespace TrueCrimeRepo.Services
+{
+    public class CrimeService
+    {
+        private readonly string _userID;
+        private ApplicationUser _user;
+
+        public CrimeService(string userID)
+        {
+            _userID = userID;
+        }
+        public bool CreateCrime(CrimeCreate model)
+        {
+            var entity =
+                new Crime()
+                {
+                    UserId = _userID,
+                    Title = model.Title,
+                    Description = model.Description,
+                    Year = model.Year,
+                    Perpetrator = model.Perpetrator,
+                    Location = model.Location,
+                    IsSolved = model.IsSolved,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Crimes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public IEnumerable<CrimeListItem> GetCrimes()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Crimes
+                        .Where(e => e.UserId == _userID)
+                        .Select(
+                            e =>
+                                new CrimeListItem
+                                {
+                                    //CrimeID = e.CrimeID,
+                                    Title = e.Title,
+                                    Description = e.Description,
+                                    Year = e.Year,
+                                    Perpetrator = e.Perpetrator,
+                                    Location = e.Location,
+                                    IsSolved = e.IsSolved,
+                                    
+                                }
+                        );
+
+                return query.ToArray();
+            }
+        }
+
+    }
+}
