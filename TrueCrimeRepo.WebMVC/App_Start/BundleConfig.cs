da5aba74fb2ea04019dc244bfe849bc889ff4e63
ericaterkhorn <erica.terkhorn@gmail.com>
2021-01-21 22:24:44 -0500

    bootswatch first updates

--- TrueCrimeRepo.WebMVC/App_Start/BundleConfig.cs
+++ TrueCrimeRepo.WebMVC/App_Start/BundleConfig.cs
@@ -23,8 +23,8 @@ namespace TrueCrimeRepo.WebMVC
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Bootswatchslatetheme.css",
+                      "~/Content/Site.css"));
         }
     }
 }
